ff2454732319e52c5f06ff24d2ab2e48f53b57c3
KSC0126 <16812502+KSC0126@users.noreply.github.com>
2022-02-10 12:27:02 -0500

    Moved everything to JS

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./assets/tailwind.css";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App).use(store).use(router);
+app.use(VueAxios, axios);
+app.mount("#app");
